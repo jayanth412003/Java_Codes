caf8f0df465459ed7f4236bfb801b421429c5d53
jayanth412003 <lakkojujayanth412003@gmail.com>
2026-06-15 19:35:47 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void Main(String[]args){
+        scanner sc=new Scanner(system.in);
+        system.out.print("enter a character:");
+        char ch=sc.new().charAt(0);
+        if(ch=='a'||ch=='e'||ch=='i'||ch='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch='O'||ch=='U'||){
+            system.out.println("vowel");
+        } else{
+            system.out.println("consontet")
+        }
+        }
+    }
+}
